ad487d4f965d5c12d0e124a08c6af29084cd1248
Carlos Ruiz <carlos3841xqgmail.com>
2025-09-25 23:41:37 -0500

    Suma

--- inicio.py
+++ inicio.py
@@ -1,3 +1,11 @@
 #nombre : Santiago Guerrero ,Carlos ruiz ,Jhoanthan Molina , Carlos peñaranda
 #Materia : Buenas Practicas 
 #Actividad de operaciones basicas colaborativas en git 
+
+def suma():
+    print("Operación: Suma")
+    numero1 = int(input("Ingrese el primer número: "))
+    numero2 = int(input("Ingrese el segundo número: "))
+    return numero1 + numero2
+
+print(suma())
