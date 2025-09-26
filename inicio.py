50e525f32b2b738258702e64a36a95c0f8289d53
Carlos Ruiz <carlos3841xqgmail.com>
2025-09-25 23:39:34 -0500

    Division

--- inicio.py
+++ inicio.py
@@ -1,3 +1,11 @@
 #nombre : Santiago Guerrero ,Carlos ruiz ,Jhoanthan Molina , Carlos peñaranda
 #Materia : Buenas Practicas 
 #Actividad de operaciones basicas colaborativas en git 
+
+def division():
+    print("Operación: División")
+    numero1 = int(input("Ingrese el primer número: "))
+    numero2 = int(input("Ingrese el segundo número: "))
+    return numero1 / numero2
+
+print(division())
