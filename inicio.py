6e9999fa71116dbac5d8ca29b4d47c4441ef1f6b
Carlos Ruiz <carlos3841xqgmail.com>
2025-09-25 23:50:36 -0500

    Resta

--- inicio.py
+++ inicio.py
@@ -1,3 +1,11 @@
 #nombre : Santiago Guerrero ,Carlos ruiz ,Jhoanthan Molina , Carlos peñaranda
 #Materia : Buenas Practicas 
 #Actividad de operaciones basicas colaborativas en git 
+
+def resta():
+    print("Operación: Resta")
+    numero1 = int(input("Ingrese el primer número: "))
+    numero2 = int(input("Ingrese el segundo número: "))
+    return numero1 - numero2
+
+print(resta())
